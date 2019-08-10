c9396892daff4e8e9335361239d8d8e31f23a1a6
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-08-10 00:38:24 -0500

    Remove dependencies

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
-import { double } from './double'
+const canvas = document.createElement('canvas')
+canvas.id = 'canvas'
+document.body.appendChild(canvas)
 
-console.log('test', double(10))
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+    // Filled triangle
+    ctx.beginPath();
+    ctx.moveTo(25, 25);
+    ctx.lineTo(105, 25);
+    ctx.lineTo(25, 105);
+    ctx.fill();
+
+    // Stroked triangle
+    ctx.beginPath();
+    ctx.moveTo(125, 125);
+    ctx.lineTo(125, 45);
+    ctx.lineTo(45, 125);
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+draw()
