dfa2e9aa79b29a5f42ae65d97750a3ebb857de73
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-08-10 17:42:01 -0500

    Add distance formula

--- js/railbike/index.js
+++ js/railbike/index.js
@@ -224,6 +224,11 @@ export class Sketch {
     this.ctx.stroke()
   }
   // Shape Machine (END) --------------------------------------------------//
+
+  dist(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) +
+                     Math.pow(y2 - y1, 2))
+  }
 }
 
 // TODO: Use this somewhere? Might need adjustments to mouse position?
