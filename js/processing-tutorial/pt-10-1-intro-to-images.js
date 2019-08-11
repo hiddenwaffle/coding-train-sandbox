ad4acfd4f7d6aa069335f7e7f6cebe2078cb89da
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-08-11 12:14:00 -0500

    Draw on image

--- js/processing-tutorial/pt-10-1-intro-to-images.js
+++ js/processing-tutorial/pt-10-1-intro-to-images.js
@@ -3,12 +3,14 @@ import violetUrl from '/assets/violet.jpg'
 
 const q = new Sketch()
 q.size(360, 240)
-q.stroke(255, 255, 0)
 
 function start(violet) {
   q.draw = () => {
     q.background(0)
     q.image(violet, 0, 0)
+
+    q.fill(0, 255, 0)
+    q.ellipse(300, 200, 10, 10)
   }
 }
 
