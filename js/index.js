37fb4e602ffbf5f965f863a0d069ee446839b56e
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-08-11 09:27:00 -0500

    Fix

--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
 // import './processing-tutorial/pt-10-1-intro-to-images'
-import './noc/noc-0-3-gaussian-distribution-2'
+import './noc/noc-0-4-custom-distribution'
--- js/noc/noc-0-4-custom-distribution.js
+++ js/noc/noc-0-4-custom-distribution.js
@@ -4,20 +4,22 @@ const q = new Sketch()
 // https://www.youtube.com/watch?v=DfziDXHYoik
 
 q.size(400, 300)
-const vals = []
-const norms = []
+const vals = new Array(q.width).fill(0)
+const norms = new Array(q.width).fill(0)
 
 q.draw = () => {
   q.background(100)
 
-  const n = montecarlo()
+  for (let i = 0; i < 25; i++) { // Make it go faster
+    const n = montecarlo()
+    const index = Math.floor(n * q.width)
+    vals[index]++
+  }
 
-  const index = Math.floor(n * q.width)
-  vals[index]++
   q.stroke(255)
 
-  const normalization = false
-  const maxy = 0
+  let normalization = false
+  let maxy = 0
 
   for (let x = 0; x < vals.length; x++) {
     q.line(x, q.height, x, q.height - norms[x])
