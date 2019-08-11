f9971e24a48600ad4c032605b15abc84d82a0fe5
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-08-11 13:44:11 -0500

    Set up test data before committing

--- js/processing-tutorial/pt-10-1-intro-to-images.js
+++ js/processing-tutorial/pt-10-1-intro-to-images.js
@@ -8,15 +8,15 @@ function start(violet) {
   q.draw = () => {
     q.background(128)
     q.fill(0, 255, 255)
-    q.ellipse(250, 200, 40, 40)
+    q.ellipse(250, 200, 140, 140)
 
     q.tint(0, 153, 204)
-    // q.tint(255, 128) // This should make it transparent
+    q.tint(255, 128) // This should make it transparent
     // q.image(violet, 10, 10, q.mouseX, q.mouseY)
     q.image(violet, q.width / 4, q.height / 4)
 
     q.fill(0, 255, 0)
-    q.ellipse(150, 150, 20, 20)
+    q.ellipse(100, 150, 60, 60)
   }
 }
 
