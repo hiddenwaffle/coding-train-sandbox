8b135f5e5aa1e394437283409caa15b1ce889407
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-08-13 11:57:26 -0500

    Finish l system

--- js/noc/noc-8-5-l-systems.js
+++ js/noc/noc-8-5-l-systems.js
@@ -1,51 +1,92 @@
 import { Sketch } from '../handcar'
 const s = new Sketch()
+s.size(600, 600)
 
-// let current = 'A'
-// let count = 0
+// https://www.youtube.com/watch?v=f6ra024-ASY
 
-// s.size(600, 600)
-// const ruleset = {
-//   'F': 'FF+[+F-F-F]-[-F+F+F]'
-// }
-// const lsys = new LSystem('F', ruleset)
-// const turtle = new Turtle(lsys.getSentence(), s.height / 3, s.radians(25))
+class LSystem {
+  constructor(sentence, ruleset) {
+    this.sentence = sentence
+    this.ruleset = ruleset
+  }
 
-// s.draw = () => {
-//   s.background(255)
-//   s.fill(0)
-//   s.translate(s.width / 2, s.height)
-//   s.rotate(-Math.PI / 2)
-//   turtle.render()
-//   s.noLoop()
-// }
+  generate() {
+    let next = ''
+    for (let c of this.sentence) {
+      const result = this.ruleset[c]
+      if (result) {
+        next += result
+      } else {
+        next += c
+      }
+    }
+    this.sentence = next
+  }
+}
 
-// let counter = 0
+class Turtle {
+  constructor(todo, len, theta) {
+    this.todo = todo
+    this.len = len
+    this.theta = theta
+  }
 
-// s.mousePressed = () => {
-//   if (counter < 5) {
-//     s.pushMatrix()
-//     lsys.generate()
-//     turtle.setToDo(lsys.getSentence())
-//     turtle.changeLen(0.5)
-//     s.popMatrix()
-//     s.redraw()
-//     counter++
-//   }
-// }
+  render() {
+    s.stroke(0, 175)
+    for (let c of this.todo) {
+      switch (c) {
+        case 'F':
+        case 'G':
+          s.line(0, 0, this.len, 0)
+          s.translate(this.len, 0)
+          break
+        case '+':
+          s.rotate(this.theta)
+          break
+        case '-':
+          s.rotate(-this.theta)
+          break
+        case '[':
+          s.pushMatrix()
+          break
+        case ']': {
+          s.popMatrix()
+          break
+        }
+      }
+    }
+  }
 
-let x = 25
+  changeLen(pct) {
+    this.len *= pct
+  }
+}
+
+const ruleset = {
+  'F': 'FF+[+F-F-F]-[-F+F+F]'
+}
+const lsys = new LSystem('F', ruleset)
+const turtle = new Turtle(lsys.sentence, s.height / 3, s.radians(25))
 
 s.draw = () => {
   s.background(255)
-  s.fill(128)
-  s.stroke(0)
-  s.ellipse(x, 75, 50, 50)
+  s.fill(0)
+  s.translate(s.width / 2, s.height)
+  s.rotate(-Math.PI / 2)
+  turtle.render()
   s.noLoop()
-  console.log(x)
 }
 
+let counter = 0
+
 s.mousePressed = () => {
-  x++
-  s.redraw()
+  if (counter < 5) {
+    s.pushMatrix()
+    lsys.generate()
+    turtle.todo = lsys.sentence
+    turtle.changeLen(0.5)
+    s.popMatrix()
+    s.redraw()
+    counter++
+  }
 }
