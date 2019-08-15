0623c4266bd1756ca54a0eaedced31e846ec6a07
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-08-15 17:37:16 -0500

    Finish fractal trees

--- js/cc/cc-014-fractal-trees/branch.js
+++ js/cc/cc-014-fractal-trees/branch.js
@@ -5,15 +5,34 @@ export class Branch {
   constructor(begin, end) {
     this.begin = begin
     this.end = end
+    this.finished = false
+  }
+
+  jitter() {
+    this.end.x += s.random(-1, 1)
+    this.end.y += s.random(-1, 1)
   }
 
   show() {
     s.stroke(255)
-    s.line(this.begin.x, this.begin.y, this.begin.x, this.end.y)
+    s.line(this.begin.x, this.begin.y, this.end.x, this.end.y)
   }
 
-  branch() {
-    // const dir = new Vector()
-    // const right = new Branch(this.begin.x, this.begin.y)
+  branchA() {
+    const dir = Vector.sub(this.end, this.begin)
+    dir.rotate(Math.PI / 6)
+    dir.mult(0.67)
+    const newEnd = Vector.add(this.end, dir)
+    const right = new Branch(this.end, newEnd)
+    return right
+  }
+
+  branchB() {
+    const dir = Vector.sub(this.end, this.begin)
+    dir.rotate(-Math.PI / 6)
+    dir.mult(0.67)
+    const newEnd = Vector.add(this.end, dir)
+    const left = new Branch(this.end, newEnd)
+    return left
   }
 }
--- js/cc/cc-014-fractal-trees/fractal-trees.js
+++ js/cc/cc-014-fractal-trees/fractal-trees.js
@@ -2,14 +2,45 @@ import s from './fractal-trees-sketch'
 import { Vector } from '../../handcar'
 import { Branch } from './branch'
 
-const tree = []
+let tree = []
+let leaves = []
 const a = new Vector(s.width / 2, s.height)
 const b = new Vector(s.width / 2, s.height - 100)
-tree.push(new Branch(a, b))
+const root = new Branch(a, b)
+tree.push(root)
+
+let count = 0
+
+s.mousePressed = () => {
+  const newBranches = []
+  for (let branch of tree) {
+    if (!branch.finished) {
+      newBranches.push(branch.branchA())
+      newBranches.push(branch.branchB())
+      branch.finished = true
+    }
+  }
+  tree = tree.concat(newBranches)
+  count++
+  if (count === 6) {
+    for (let branch of tree) {
+      if (!branch.finished) {
+        leaves.push(branch.end.copy())
+      }
+    }
+  }
+}
 
 s.draw = () => {
   s.background(51)
   for (let branch of tree) {
     branch.show()
+    // branch.jitter()
+  }
+  for (let leaf of leaves) {
+    s.fill(255, 0, 100, 100)
+    s.noStroke()
+    s.ellipse(leaf.x, leaf.y, 8, 8)
+    leaf.y += s.random(0, 1)
   }
 }
